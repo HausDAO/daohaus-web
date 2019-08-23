eb93a876846b6d709f0a50fbe791c545924e757a
dekan <dekanbro@gmail.com>
2019-08-23 16:21:08 -0600

    initial web3

--- src/App.js
+++ src/App.js
@@ -4,8 +4,25 @@ import Routes from './Routes';
 import logo from "./logo.svg";
 import "./App.css";
 
+import { Connectors } from 'web3-react'
+import Web3Provider from 'web3-react'
+const { InjectedConnector, NetworkOnlyConnector } = Connectors
+
+const MetaMask = new InjectedConnector({ supportedNetworks: [1, 4] })
+
+const Infura = new NetworkOnlyConnector({
+  providerURL: 'https://mainnet.infura.io/v3/...'
+})
+
+const connectors = { MetaMask, Infura }
+
+
+
 function App() {
   return (
+    <Web3Provider
+      connectors={connectors}
+      libraryName={'ethers.js'|'web3.js'|null}>
     <div className="App">
       <Router>
         <header className="App-header">
@@ -14,6 +31,7 @@ function App() {
         <Routes />
       </Router>
     </div>
+     </Web3Provider>
   );
 }
 
--- src/views/home/Home.js
+++ src/views/home/Home.js
@@ -1,5 +1,26 @@
-import React from "react";
+import React, {useEffect} from "react";
+import { useWeb3Context } from 'web3-react'
 
-const Home = () => <h1>DAUHAUS</h1>;
+const Home = () => {
+
+    const context = useWeb3Context()
+
+    useEffect(() => {
+      context.setFirstValidConnector(['MetaMask', 'Infura'])
+    }, [])
+  
+    if (!context.active && !context.error) {
+      // loading
+      return <h1>DAUHAUS</h1>
+    } else if (context.error) {
+      //error
+      return <h1>DAUHAUS ERROR</h1>
+    } else {
+      // success
+      return <h1>DAUHAUS Success</h1>
+    }
+
+
+};
 
 export default Home;
