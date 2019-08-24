45661f6a754b23ede13ee6c27d5214d147d7e884
skuhlmann <samkuhlmann@gmail.com>
2019-08-24 13:29:17 +0200

    Adds summon button and route

--- src/Routes.js
+++ src/Routes.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
+
 import Home from "./views/home/Home";
 import Dao from "./views/dao/Dao";
+import Summon from "./views/summon/Summon";
 import FourOhFour from "./views/fourOhFour/FourOhFour";
 
 const Routes = () => (
   <Switch>
     <Route path="/" exact component={Home} />
     <Route path="/dao/:contractAddress" exact component={Dao} />
+    <Route path="/summon" exact component={Summon} />
     <Route path="*" component={FourOhFour} />
   </Switch>
 );
