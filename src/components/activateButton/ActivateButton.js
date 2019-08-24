62a7ee5f4b5b5935e27fcfba71e9fdb37784fbe6
dekan <dekanbro@gmail.com>
2019-08-24 05:10:04 -0600

    show error

--- src/components/activateButton/ActivateButton.js
+++ src/components/activateButton/ActivateButton.js
@@ -7,7 +7,12 @@ const ActivateButton = () => {
 
     const context = useWeb3Context()
 
-    const activate = () => context.setFirstValidConnector(['MetaMask', 'Infura'])
+    const activate = async () => {
+        await context.setFirstValidConnector(['MetaMask'])
+        console.log(context);
+        
+
+    }
   
     if (!context.active && !context.error) {
       // loading
@@ -16,9 +21,15 @@ const ActivateButton = () => {
       </button>)
     } else if (context.error) {
       //error
-      return (<button onClick={() => activate()}>
-      Activate Error
-      </button>)
+      return (
+      <>
+      <button onClick={() => activate()}>
+      Activate
+      </button> 
+     
+          {context.error.code === 'UNSUPPORTED_NETWORK' &&  <p className="ErrorText">Unsupported network: please use mainnet</p>}
+      </>
+      )
     } else {
       // success
       return (<button onClick={() => activate()}>
