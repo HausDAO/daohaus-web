f044ce69bbcd9b49190843b37345f62b792a5097
skuhlmann <samkuhlmann@gmail.com>
2019-08-25 10:37:24 +0200

    Adds 3box hover

--- src/components/applicationList/ApplicationList.js
+++ src/components/applicationList/ApplicationList.js
@@ -1,17 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import ProfileHover from "profile-hover";
+
+import "./ApplicationList.scss";
 
 const ApplicationList = props => {
   const { applications } = props;
 
   const applicationList = applications.map((application, i) => {
     return (
-      <div key={i}>
+      <div key={i} className="Applicant__List">
         <Link to={`/profile/${application.applicantAddress}`}>
           <p>{application.applicantAddress}</p>
           <p>{application.name}</p>
           <p>{application.bio}</p>
         </Link>
+        <ProfileHover address={application.applicantAddress} />
       </div>
     );
   });
