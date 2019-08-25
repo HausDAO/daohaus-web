e8d89d56b5ffd0aa56c1f6b7bede6a04c66c542a
skuhlmann <samkuhlmann@gmail.com>
2019-08-25 10:40:01 +0200

    3box on the other list

--- src/components/applicationList/ApplicationShortList.js
+++ src/components/applicationList/ApplicationShortList.js
@@ -2,16 +2,18 @@ import React from "react";
 import { Link } from "react-router-dom";
 import ProfileHover from "profile-hover";
 
+import "./ApplicationList.scss";
+
 const ApplicationShortList = props => {
   const { applications } = props;
 
   const applicationList = applications.map((application, i) => {
     return (
-      <div key={i}>
+      <div key={i} className="Applicant__List">
         <Link to={`/profile/${application.applicantAddress}`}>
           <p>{application.moloch.name}</p>
         </Link>
-        <ProfileHover address={application.applicantAddres}></ProfileHover>
+        <ProfileHover address={application.applicantAddress}></ProfileHover>
       </div>
     );
   });
