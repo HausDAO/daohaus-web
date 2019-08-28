7b5f6c7a7e30bdf1408eb65ea5ed4991b6ad4a6e
dekan <dekanbro@gmail.com>
2019-08-28 17:43:46 -0600

    add balance, allownace checks

--- src/components/applicationList/ApplicationList.js
+++ src/components/applicationList/ApplicationList.js
@@ -1,11 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import ProfileHover from "profile-hover";
 
 import "./ApplicationList.scss";
+import MolochService from "../../util/molochService";
+import Web3Service from "../../util/web3Service";
+import WethService from "../../util/wethService";
+import DaiService from "../../util/daiService";
 
 const ApplicationList = props => {
-  const { applications, summoner } = props;
+  const { applications, daoData } = props;
+  const [applicants, setApplicants] = useState({});
+
+  console.log("*************daoDATA", daoData);
+
+  const currentBalance = async applicant => {
+    const molochService = new MolochService(daoData.contractAddress);
+    const web3Service = new Web3Service();
+    const wethService = new WethService();
+    const daiService = new DaiService();
+    const balance = {};
+    balance[applicant] = {};
+
+    const daoToken = await molochService.approvedToken();
+    if (daoToken === "Weth") {
+      console.log("WETH", applicant);
+      balance[applicant].allowance = await wethService.allowance(
+        applicant,
+        daoData.contractAddress
+      );
+      balance[applicant].allowanceEth = web3Service.fromWei(
+        balance[applicant].allowance
+      );
+
+      balance[applicant].balanceOf = await wethService.balanceOf(applicant);
+      console.log("allowance", balance[applicant].allowance);
+    } else if (daoToken === "Dai") {
+      balance[applicant].allowance = await daiService.allowance(
+        applicant,
+        daoData.contractAddress
+      );
+      balance[applicant].allowanceEth = web3Service.fromWei(
+        balance[applicant].allowance
+      );
+      balance[applicant].balanceOf = await daiService.balanceOf(applicant);
+    } else {
+      return false;
+    }
+
+    balance[applicant].cover = balance.allowance > balance.balanceOf;
+    console.log("-=-=-=-=-=-=-=-=-=-=-=");
+    console.log({ ...applicants, ...balance });
+    setApplicants({ ...applicants, ...balance });
+  };
 
   const applicationList = applications
     .sort(function(a, b) {
@@ -16,12 +63,16 @@ const ApplicationList = props => {
         application.status = "zero share member";
       }
 
-      if (application.applicantAddress === summoner) {
+      if (
+        application.applicantAddress.toLowerCase() ===
+        daoData.summonerAddress.toLowerCase()
+      ) {
         application.status = "summoner";
       }
 
       if (application.status === "new") {
         application.status = "New Pledge";
+        currentBalance(application.applicantAddress);
       }
 
       if (!application.status) {
@@ -33,6 +84,19 @@ const ApplicationList = props => {
           <Link to={`/profile/${application.applicantAddress}`}>
             <p>Shares: {application.shares}</p>
             <p>{application.status}</p>
+            {applicants[application.applicantAddress] &&
+              application.status === "New Pledge" && (
+                <>
+                  <p>
+                    allowance:{" "}
+                    {"" + applicants[application.applicantAddress].allowanceEth}
+                  </p>
+                  <p>
+                    can cover with balance:{" "}
+                    {"" + applicants[application.applicantAddress].cover}
+                  </p>
+                </>
+              )}
           </Link>
           <ProfileHover address={application.applicantAddress} />
         </div>
