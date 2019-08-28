1861401e9e102c9de47fea03123a708a65996682
dekan <dekanbro@gmail.com>
2019-08-28 15:18:23 -0600

    start

--- src/components/applicationList/ApplicationList.js
+++ src/components/applicationList/ApplicationList.js
@@ -5,18 +5,33 @@ import ProfileHover from "profile-hover";
 import "./ApplicationList.scss";
 
 const ApplicationList = props => {
-  const { applications } = props;
+  const { applications, summoner } = props;
 
-  const applicationList = applications.map((application, i) => {
-    return (
-      <div key={i} className="Applicant__List">
-        <Link to={`/profile/${application.applicantAddress}`}>
-          <p>{application.applicantAddress}</p>
-        </Link>
-        <ProfileHover address={application.applicantAddress} />
-      </div>
-    );
-  });
+  const applicationList = applications
+    .sort(function(a, b) {
+      return a.shares - b.shares;
+    })
+    .map((application, i) => {
+      application.status = "member";
+
+      if (application.shares === "0") {
+        application.status = "zero share member";
+      }
+
+      if (application.applicantAddress === summoner) {
+        application.status = "summoner";
+      }
+
+      return (
+        <div key={i} className="Applicant__List">
+          <Link to={`/profile/${application.applicantAddress}`}>
+            <p>Shares: {application.shares}</p>
+            <p>{application.status}</p>
+          </Link>
+          <ProfileHover address={application.applicantAddress} />
+        </div>
+      );
+    });
 
   return <>{applicationList}</>;
 };
