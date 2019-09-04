b2b3ebd0ea0c4e05a2474b7ad385fd8ec1e91c08
dekan <dekanbro@gmail.com>
2019-09-04 14:05:16 -0600

    update with summoner

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ import "./global.scss";
 
 const { InjectedConnector, NetworkOnlyConnector } = Connectors;
 
-// const MetaMask = new InjectedConnector({ supportedNetworks: [1,2,3,4] })
-const MetaMask = new InjectedConnector({ supportedNetworks: [1] });
+const MetaMask = new InjectedConnector({ supportedNetworks: [1,2,3,4, 42] })
+// const MetaMask = new InjectedConnector({ supportedNetworks: [1] });
 
 const Infura = new NetworkOnlyConnector({
   providerURL: process.env.REACT_APP_INFURA_URI
