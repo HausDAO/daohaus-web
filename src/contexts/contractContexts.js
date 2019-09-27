28065fc7232a98a3cb9607a0eeae9cbc5132e765
skuhlmann <samkuhlmann@gmail.com>
2019-09-27 13:10:15 -0600

    Adds a bunch of packages and linting configs

--- src/contexts/contractContexts.js
+++ src/contexts/contractContexts.js
@@ -1,11 +1,57 @@
 import React, { useState, useEffect, createContext } from "react";
 
-export const MolochContext = createContext();
+import Web3 from "web3";
 
-const contractContexts = ({ children }) => {
+import WethService from "../util/wethService";
+import DaiService from "../util/daiService";
+import Web3Service from "../util/web3Service";
+
+export const MolochContext = createContext(null);
+export const Web3Context = createContext();
+export const WethContext = createContext();
+export const DaiContext = createContext();
+
+const ContractContexts = ({ children }) => {
+  const [web3, setWeb3] = useState();
+  const [weth, setWeth] = useState();
+  const [dai, setDai] = useState();
+  const [moloch, setMoloch] = useState();
+
+  useEffect(() => {
+    const setUp = async () => {
+      let _web3;
+      if (Web3.givenProvider) {
+        console.log('reg web3');
+        
+        _web3 = new Web3(Web3.givenProvider);
+      } else {
+        console.log('net web3');
+        _web3 = new Web3(
+          new Web3.providers.HttpProvider(process.env.REACT_APP_INFURA_URI)
+        );
+      }
+      const web3Service = new Web3Service(_web3);
+      console.log('web3service', web3Service);
+      setWeb3(web3Service);
+      
+      const wethService = new WethService(web3Service);
+      const daiService = new DaiService(web3Service);
+      setWeth(wethService);
+      setDai(daiService);
+    }
+    setUp();
+  }, []);
   return (
-    <MolochContext.Provider value={[moloch, setMoloch]}>
-      {children}
-    </MolochContext.Provider>
+    <Web3Context.Provider value={[web3, setWeb3]}>
+      <MolochContext.Provider value={[moloch, setMoloch]}>
+        <DaiContext.Provider value={[dai, setDai]}>
+          <WethContext.Provider value={[weth, setWeth]}>
+            {children}
+          </WethContext.Provider>
+        </DaiContext.Provider>
+        </MolochContext.Provider>
+    </Web3Context.Provider>
   );
 };
+
+export default ContractContexts;
