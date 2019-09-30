81a34ed077efb453ebfcdc027e63c29cea29bbe1
skuhlmann <samkuhlmann@gmail.com>
2019-09-30 08:53:52 -0600

    Adds members query into the resolver

--- src/components/applicationForm/summary.js
+++ src/components/applicationForm/summary.js
@@ -1,11 +1,9 @@
-import { useFormikWizard } from 'formik-wizard'
-import React, { useState, useEffect, useContext } from 'react'
-import { withRouter } from "react-router-dom";
+import { useFormikWizard } from 'formik-wizard';
+import React, { useState, useEffect, useContext } from 'react';
+import { withRouter } from 'react-router-dom';
 
-
-import { Web3Context, MolochContext  } from "../../contexts/ContractContexts";
+import { MolochContext } from '../../contexts/ContractContexts';
 import { addressToToken } from '../../util/constants';
-
 
 function Summary(props) {
   const { values } = useFormikWizard();
@@ -14,26 +12,33 @@ function Summary(props) {
 
   useEffect(() => {
     const fetchData = async () => {
-      
-        const token = await molochContext.methods.approvedToken().call();
-        const tokenSymbol = addressToToken[token];
-        setContractData({tokenSymbol})
+      const token = await molochContext.methods.approvedToken().call();
+      const tokenSymbol = addressToToken[token];
+      setContractData({ tokenSymbol });
     };
 
     fetchData();
-  }, [props.match.params.contractAddress]);
+  }, [molochContext.methods, props.match.params.contractAddress]);
 
   return (
     <div className="Step Summary">
       <h3>Pledge to the DAO!</h3>
-      <p className="Label">Name</p><p className="Value Data">{values.personal.name}</p>
-      <p className="Label">Bio</p><p className="Value Data">{values.personal.bio}</p>
-      <p className="Value">You are pledging {values.pledge.pledge} {contractData.token} in tribute
-      and requesting {values.shares.shares} shares</p>
-      <p>After pledging a sponsoring member can now make a proposal for your application. You have not been accepted yet and have 
-        only approved the DAO to hold your funds during the proposal period. You must have these funds available in your wallet for a proposal to begin..</p>
+      <p className="Label">Name</p>
+      <p className="Value Data">{values.personal.name}</p>
+      <p className="Label">Bio</p>
+      <p className="Value Data">{values.personal.bio}</p>
+      <p className="Value">
+        You are pledging {values.pledge.pledge} {contractData.token} in tribute
+        and requesting {values.shares.shares} shares
+      </p>
+      <p>
+        After pledging a sponsoring member can now make a proposal for your
+        application. You have not been accepted yet and have only approved the
+        DAO to hold your funds during the proposal period. You must have these
+        funds available in your wallet for a proposal to begin..
+      </p>
     </div>
-  )
+  );
 }
 
-export default withRouter(Summary)
+export default withRouter(Summary);
--- src/components/memberItem/MemberItem.js
+++ src/components/memberItem/MemberItem.js
@@ -1,31 +1,21 @@
-import React, { useState, useEffect, useContext } from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
-import { getProfile } from "3box/lib/api";
-import makeBlockie from "ethereum-blockies-base64";
+import { getProfile } from '3box/lib/api';
+import makeBlockie from 'ethereum-blockies-base64';
 
-import { truncateAddr } from "../../util/helpers";
+import { truncateAddr } from '../../util/helpers';
 
-import "./MemberItem.scss";
-import {
-  WethContext,
-  DaiContext,
-  Web3Context
-} from "../../contexts/ContractContexts";
-import { addressToToken } from "../../util/constants";
+import './MemberItem.scss';
 
 const MemberItem = props => {
-  const { applicant, daoData, contract, contractData } = props;
+  const { applicant, daoData, contract } = props;
   const [currentApplicant, setCurrentApplicant] = useState([]);
-
-  const [web3Service] = useContext(Web3Context);
-  const [wethService] = useContext(WethContext);
-  const [daiService] = useContext(DaiContext);
 
   useEffect(() => {
     const setup = async () => {
-      if (applicant.id.split("-")[1] && contract) {
-        const _applicant = applicant.id.split("-")[1];
+      if (applicant.id.split('-')[1] && contract) {
+        const _applicant = applicant.id.split('-')[1];
 
         let profile;
         try {
@@ -34,43 +24,41 @@ const MemberItem = props => {
           profile = {};
         }
         setCurrentApplicant(currentApplicant => [
-            ...currentApplicant,
-            { addr: _applicant, profile }
-          ]);
+          ...currentApplicant,
+          { addr: _applicant, profile },
+        ]);
 
         return true;
       } else {
         setCurrentApplicant(currentApplicant => [
           ...currentApplicant,
-          { addr: {}, profile: {} }
+          { addr: {}, profile: {} },
         ]);
 
         return false;
       }
     };
     setup();
-  }, [applicant]);
+  }, [applicant, contract]);
 
   if (
-    applicant.id.split("-")[1].toLowerCase() ===
+    applicant.id.split('-')[1].toLowerCase() ===
     daoData.summonerAddress.toLowerCase()
   ) {
-    applicant.status = "Summoner";
+    applicant.status = 'Summoner';
   } else {
-    applicant.status = "Member";
+    applicant.status = 'Member';
   }
 
-
   const applicantProfile = currentApplicant.find(
-    item => item.addr === applicant.id.split("-")[1]
+    item => item.addr === applicant.id.split('-')[1],
   );
 
   return (
-    <Link to={`/profile/${applicant.id.split("-")[1]}`}>
+    <Link to={`/profile/${applicant.id.split('-')[1]}`}>
       <div className="Row MemberInfo">
         <p>{applicant.status}</p>
         <p>{applicant.shares} Shares</p>
-
       </div>
       <div className="Row ProfileInfo">
         {applicantProfile &&
@@ -80,22 +68,22 @@ const MemberItem = props => {
           <div
             className="ProfileImgCard"
             style={{
-              backgroundImage: `url(${"https://ipfs.infura.io/ipfs/" +
-                applicantProfile.profile.image[0].contentUrl["/"]})`
+              backgroundImage: `url(${'https://ipfs.infura.io/ipfs/' +
+                applicantProfile.profile.image[0].contentUrl['/']})`,
             }}
           >
-            {""}
+            {''}
           </div>
         ) : (
           <div
             className="ProfileImgCard"
             style={{
               backgroundImage: `url("${makeBlockie(
-                applicant.id.split("-")[1]
-              )}")`
+                applicant.id.split('-')[1],
+              )}")`,
             }}
           >
-            {""}
+            {''}
           </div>
         )}
         <div>
@@ -103,13 +91,13 @@ const MemberItem = props => {
           applicantProfile.profile &&
           applicantProfile.profile.name ? (
             <h2>
-              {applicantProfile.profile.name}{" "}
+              {applicantProfile.profile.name}{' '}
               {applicantProfile.profile.emoji ? (
                 <span>{applicantProfile.profile.emoji} </span>
               ) : null}
             </h2>
           ) : null}
-          <p>{truncateAddr(applicant.id.split("-")[1])}</p>
+          <p>{truncateAddr(applicant.id.split('-')[1])}</p>
         </div>
       </div>
     </Link>
