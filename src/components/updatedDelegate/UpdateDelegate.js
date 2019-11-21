abb05d38946c391e63e577ccf0ea4dfbf2ed60f1
dekan <dekanbro@gmail.com>
2019-11-21 13:25:15 -0700

    add notification on form

--- src/components/updatedDelegate/UpdateDelegate.js
+++ src/components/updatedDelegate/UpdateDelegate.js
@@ -1,52 +1,52 @@
-import React from "react";
+import React from 'react';
 
-import { Formik, Form, Field, ErrorMessage } from "formik";
-import { useWeb3Context } from "web3-react";
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { useWeb3Context } from 'web3-react';
 
 // import Loading from '../shared/Loading';
 
 const UpdateDelegate = ({ contract }) => {
   const context = useWeb3Context();
+  const [formError, setformError] = useState('');
 
   return (
     <>
       {/* {loading && <Loading />} */}
 
       <h2>Update Delegate Form</h2>
+      {formError && <small>{formError}</small>}
+
       <Formik
         initialValues={{
-          address: "",
+          address: '',
         }}
         validate={values => {
           let errors = {};
           if (!values.address) {
-            errors.address = "Required";
+            errors.address = 'Required';
           }
           return errors;
         }}
         onSubmit={async (values, { setSubmitting, resetForm }) => {
-          
           // setLoading(true);
           try {
-            
             await contract.methods
-            .updateDelegateKey(
-              values.address
-            )
-            .send({ from: context.account })
-            .once("transactionHash", txHash => {})
-            .on("receipt", async (receipt) => {
-              console.log('receipt', receipt); 
-            })
-            .then(resp => {
-              return resp;
-            })
-            .catch(err => {
-              //setLoading(false);
-              console.log(err);
-              return { error: "rejected transaction" };
-            });
-           
+              .updateDelegateKey(values.address)
+              .send({ from: context.account })
+              .once('transactionHash', txHash => {
+                setformError(`transaction sent hash: `, txHash);
+              })
+              .on('receipt', async receipt => {
+                console.log('receipt', receipt);
+              })
+              .then(resp => {
+                return resp;
+              })
+              .catch(err => {
+                //setLoading(false);
+                console.log(err);
+                return { error: 'rejected transaction' };
+              });
           } catch (err) {
             console.log(err);
             alert(`Something went wrong. please try again`);
@@ -61,7 +61,7 @@ const UpdateDelegate = ({ contract }) => {
           <Form className="Form">
             <Field name="address">
               {({ field, form }) => (
-                <div className={field.value ? "Field HasValue" : "Field "}>
+                <div className={field.value ? 'Field HasValue' : 'Field '}>
                   <label>Address</label>
                   <input type="text" {...field} />
                 </div>
@@ -75,7 +75,6 @@ const UpdateDelegate = ({ contract }) => {
             <button type="submit" disabled={isSubmitting}>
               Update Delegate
             </button>
-
           </Form>
         )}
       </Formik>
