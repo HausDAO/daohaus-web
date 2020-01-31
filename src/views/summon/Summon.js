7afe47180ea8cb6eda50d67b6dc6a2a5d72288d4
Dekan <dekanbro@gmail.com>
2020-01-31 13:38:51 -0700

    add v2 advanced form

--- src/views/summon/Summon.js
+++ src/views/summon/Summon.js
@@ -3,13 +3,19 @@ import { useWeb3Context } from "web3-react";
 
 import SummonAdvForm from "../../components/summonAdvForm/SummonAdvForm";
 import SummonWizard from "../../components/summonWizard/SummonWizard";
+import SummonAdvV2Form from "../../components/summonAdvV2Form/SummonAdvV2Form";
 
 const Summon = () => {
   const context = useWeb3Context();
   const [wizardForm, setWizardForm] = useState(true);
+  const [v2Form, setV2Form] = useState(true);
 
   const toggleForm = () => {
     setWizardForm(!wizardForm);
+  };
+
+  const toggleV2 = () => {
+    setV2Form(!v2Form);
   };
 
   return (
@@ -17,16 +23,25 @@ const Summon = () => {
       {context.account ? (
         <div className="View SmallContainer">
           <div className="Row">
-          <p>{wizardForm ? "Summon (Easy Mode)" : "Summon (Hard Mode)"}</p>
-          <button className="TabButton" onClick={toggleForm}>
-            {wizardForm ? "Hard Mode" : "Easy Mode"}
-          </button>
+            <button className="TabButton" onClick={toggleForm}>
+              {wizardForm ? "Hard Mode" : "Easy Mode"}
+            </button>
+            <p>{wizardForm ? "Summon (Easy Mode)" : "Summon (Hard Mode)"}</p>
           </div>
           {wizardForm ? (
             <SummonWizard></SummonWizard>
           ) : (
-            <SummonAdvForm></SummonAdvForm>
-          )}
+              <div>
+                <p>{v2Form ? "Version 2" : "Version 1"}</p>
+
+                <button className="TabButton" onClick={toggleV2}>
+                  Switch to {v2Form ? "Version 1" : "Version 2"}
+                </button>
+
+                {v2Form ? (<SummonAdvV2Form></SummonAdvV2Form>) : (<SummonAdvForm></SummonAdvForm>)}
+
+              </div>
+            )}
         </div>
       ) : null}
     </>
