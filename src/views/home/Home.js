86590d1ae5240ff1533456acacf7f5a89129b257
skuhlmann <samkuhlmann@gmail.com>
2020-01-31 14:21:31 -0700

    home page radio buttons

--- src/views/home/Home.js
+++ src/views/home/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useWeb3Context } from 'web3-react';
 import { useQuery } from '@apollo/react-hooks';
 import _ from 'lodash';
@@ -12,6 +12,7 @@ import HeroBackground from '../../assets/daohaus__hero--falling.png';
 import './Home.scss';
 
 const Home = () => {
+  const [molochVersion, setMolochVersion] = useState('1');
   const context = useWeb3Context();
   const { loading, error, data } = useQuery(GET_MOLOCHES);
   const filterDaos = daos => {
@@ -34,18 +35,40 @@ const Home = () => {
           <br />
           Haus of Daos
         </h1>
-        <h2>
-          Discover and pledge to join existing daos.
-        </h2>
-        <h2>
-          Or summon your own.
-        </h2>
+        <h2>Discover and pledge to join existing daos.</h2>
+        <h2>Or summon your own.</h2>
         {context.active && !context.error ? (
           <SummonButton />
         ) : (
           <ActivateButton msg={'Sign in'} />
         )}
       </div>
+      {data ? (
+        <>
+          <div className="radio">
+            <label>
+              <input
+                type="radio"
+                value="1"
+                checked={molochVersion === '1'}
+                onChange={() => setMolochVersion('1')}
+              />
+              Moloch V1
+            </label>
+          </div>
+          <div className="radio">
+            <label>
+              <input
+                type="radio"
+                value="2"
+                checked={molochVersion === '2'}
+                onChange={() => setMolochVersion('2')}
+              />
+              Moloch V2
+            </label>
+          </div>
+        </>
+      ) : null}
       <div className="Block Primary">
         {loading ? <p className="View">Loading DAOs</p> : null}
         {error ? <p className="View">Sorry there's been an error</p> : null}
