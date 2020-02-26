4da668fd26921025686a38b2d412b84355a1e07d
Dekan <dekanbro@gmail.com>
2020-02-25 19:36:49 -0700

    hot fix default weth address

--- src/components/summonAdvV2Form/SummonAdvV2Form.js
+++ src/components/summonAdvV2Form/SummonAdvV2Form.js
@@ -32,7 +32,7 @@ const SummonAdvV2Form = props => {
             initialValues={{
               name: '',
               description: '',
-              approvedTokens: '0xd0a1e359811322d97991e03f863a0c30c2cf029c',
+              approvedTokens: ''+process.env.REACT_APP_WETH_ADDRESS,
               periodDuration: '',
               votingPeriodLength: '',
               gracePeriodLength: '',
