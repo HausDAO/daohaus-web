67698e61a0abbfeb9bd1150ff39bb001894fe2d8
Dekan <dekanbro@gmail.com>
2020-08-18 17:36:15 -0600

    update xdai currency input to not be read only

--- src/util/chains.js
+++ src/util/chains.js
@@ -75,7 +75,7 @@ const supportedChains = [
     name: 'xDAI Chain',
     short_name: 'xdai',
     chain: 'xDAI',
-    network: 'mainnet',
+    network: 'xDAI',
     chain_id: 100,
     network_id: 1,
     rpc_url: 'https://dai.poa.network',
