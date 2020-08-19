6d29b11450e248027f7611759a216bbb068580e3
Tarun Bansal <tarunbhm@gmail.com>
2020-08-19 17:04:53 +0530

    Add MinionNFT contract and it's abi to be used in feature

--- src/contracts/source/minions/MinionNFTMinter.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.5.0;
-
-
-contract IMinionNFTMinter {
-    function mint(to: address, tokenId: uint256) public returns (bool)
-    function mintWithTokenURI(to: address, tokenId: uint256, tokenURI: string) public returns (bool)
-    function registerNFT(nft: address) public returns (bool)
-    function deregisterNFT(nft: address) public returns (bool)
-}
